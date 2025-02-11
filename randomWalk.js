c2a516d07436d903b7b9f5508894397b43cb7f50
tagaism <i77tag@gmail.com>
2025-02-11 20:31:46 +0900

    7/30 Added forces vector (gravity, wind)

--- randomWalk.js
+++ randomWalk.js
@@ -1,12 +1,32 @@
-let walker;
+let ball;
 
 function setup() {
-    createCanvas(1000, 900);
-    walker = new Walker(width / 2, height / 2);
+    createCanvas(1500, 900);
+    ball = new Walker(3, 2, 2);
+    ball1 = new Walker(width - 4, 4, 4);
     background(0);
 }
 
 function draw() {
-    walker.update();
-    walker.show();
+    
+    
+    if(mouseIsPressed) {
+        let wind = createVector(-0.3, 0);
+        ball.applyForce(wind);
+        ball1.applyForce(wind);
+    }
+    let gravity = createVector(0, 0.1);
+
+    let weight = p5.Vector.mult(gravity, ball.mass);
+    let weight1 = p5.Vector.mult(gravity, ball1.mass);
+
+    ball.applyForce(weight);
+    ball1.applyForce(weight1);
+
+    ball.update();
+    ball1.update();
+    ball.edges();
+    ball1.edges(    );
+    ball.show();
+    ball1.show();
 }
--- walker.js
+++ walker.js
@@ -1,43 +1,42 @@
 class Walker {
-    constructor(x, y) {
+    constructor(x, y, m) {
         this.pos = createVector(x,y);
-        // random 2D
-        //
-        let angle = Math.random() * Math.PI;
-        let xx = Math.cos(angle);
-        let yy = Math.sin(angle);
-        this.vel = createVector(xx, yy);
-        //
-        // random 2D
-        this.vel.mult(random(3));
+        this.vel = createVector(0, 0);
+        this.acc =  createVector(0, 0);
+        this.mass = m;
+        this.r = sqrt(this.mass) * 10;
+    }
+
+    applyForce(force) {
+        let f = force.div(this.mass);
+        this.acc.add(f);
+    }
+
+    edges() {
+        if(this.pos.y >= height - this.r) {
+            this.pos.y = height - this.r;
+            this.vel.y *= -1;
+        }
+        if(this.pos.x >= width - this.r) {
+            this.pos.x = width - this.r;
+            this.vel.x *= -1;
+        }
+        if(this.pos.x <= 0 + this.r) {
+            this.pos.x = this.r;
+            this.vel.x *= -1;
+        }
     }
 
     update() {
-        let mouse = createVector(mouseX, mouseY);
-    
-        // random 2D
-        //
-        let angle = Math.random() * Math.PI;
-        let xx = Math.cos(angle);
-        let yy = Math.sin(angle);
-        this.acc = mouse.sub(this.pos);
-        //
-        // random 2D
-
-
-        this.vel.setMag(1)
-        // this.acc.setMat(0.1);
         this.vel.add(this.acc);
-        this.vel.limit(5);
-
-        // this.vel.limit(2);
         this.pos.add(this.vel);
+        this.acc.set(0, 0);
     }
 
     show() {
-        stroke(255);
+        stroke(random(255), random(255), random(255));
         strokeWeight(2);
-        fill(255, 100);
-        ellipse(this.pos.x, this.pos.y, 32);
+        fill(100, 100);
+        ellipse(this.pos.x, this.pos.y, this.r * 2);
     }
 }
