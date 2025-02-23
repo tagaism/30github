32e2de77503f19e327ba07ff797611990b5c980b
tagaism <i77tag@gmail.com>
2025-02-23 23:13:51 +0900

    19/30 oscillation

--- oscillation.js
+++ oscillation.js
@@ -1,32 +1,40 @@
-let angles = [];
-let angleV = 0.05;
-let r = 8;
+class Wave {
+    constructor(amp, period, phase) {
+        this.amplitude = amp;
+        this.period = period;
+        this.phase = phase;
+    }
 
+    evaluate(x) {
+        console.log(x);
+        return sin(this.phase + TWO_PI * x / this.period) * this.amplitude;
+    }
+
+    update(x) {
+        this.phase += x;
+    }
+}
+
+let waves = [];
 function setup() {
     createCanvas(1700, 800);
-    let total = floor(width / (r * 2));
-    for(let i = 0; i < total + 1; i++) {
-        angles[i] = map(i, 0, total, 0, 2 * TWO_PI);
-        angleV[i] = 0.01 + i / 100;
+    for(let i = 0; i < 20; i++) {
+        waves[i] = new Wave(random(20, 80), random(100, 200), random(TWO_PI));
     }
 }
 
 function draw() {
-    background(0, 1);
-    translate(850, 400);
-    // fill(252, 234, 24);
-    noFill();
-    stroke(252, 234, 24);
-    beginShape;
-    for(let i = 0; i < angles.length; i++) {
-        let y = map(sin(angles[i]), -1, 1, -400, 400);
-        strokeWeight(4);
-        let x = map(i, 0, angles.length, -850, 850);
-        // line(x, 0, x, y);
-        // circle(x, y, r * 2);
-        vertex(x, y);
-        angles[i] += 0.02;
-        // angles[i] += angleV[i];
+    background(0);
+    for(let x = 0; x < width; x+=10) {
+        let y = 0;
+        for(let wave of waves) {
+            y += wave.evaluate(x);
+            // wave.update();
+        }
+        // noStroke();
+        ellipse(x, y + height/2, 10);
     }
-    // endShape;
+    for(let wave of waves){
+        wave.update(0.1);
+    }
 }
---
+++ pendulumSketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(1700, 800);
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+    strokeWeight(2);
+    line(850, 0, 850, 400);
+    circle(850, 400, 43);
+}
