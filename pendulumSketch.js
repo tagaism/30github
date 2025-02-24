901b4b2d8528001a686b7034c85936723550bb63
tagaism <i77tag@gmail.com>
2025-02-24 21:28:18 +0900

    20/30 Pendulum

--- pendulumSketch.js
+++ pendulumSketch.js
@@ -1,11 +1,31 @@
+let angle;
+
+let angleV = 0;
+let angleA = 0.0001;
+
+let bob;
+let len;
+let origin;
+let gravity = 1;
 function setup() {
     createCanvas(1700, 800);
+    origin = createVector(850, 0);
+    angle = PI/2.2;
+    bob = createVector();
+    len = 500;
 }
 
 function draw() {
     background(0);
+    let force = gravity * sin(angle) / len;
+    angleA = -1 * force;
+    angleV += angleA;
+    angle += angleV;
+    bob.x = len * sin(angle) + origin.x;
+    bob.y = len * cos(angle) + origin.y;
+
     stroke(255);
     strokeWeight(2);
-    line(850, 0, 850, 400);
-    circle(850, 400, 43);
-}
+    line(origin.x, origin.y, bob.x, bob.y);
+    circle(bob.x, bob.y, 43);
+} 
